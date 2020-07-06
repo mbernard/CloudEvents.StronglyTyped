bac9f87860066dd7cadb7d9ff19dc61695679455
Miguel Bernard <miguel.bernard@nexusinno.com>
2020-07-06 16:40:09 -0400

    Move files to correct folder level Add tests

---
+++ src/CloudEvents.Typed.Tests.Unit/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace CloudEvents.Typed.Tests.Unit
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
